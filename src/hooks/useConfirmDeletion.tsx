44d84f4471b57268996a4474b334fab3c0c0867e
Franco Traversaro <franco.traversaro@nan.srl>
2024-01-29 19:48:45 +0100

    Fix DELETE button

--- src/hooks/useConfirmDeletion.tsx
+++ src/hooks/useConfirmDeletion.tsx
@@ -1,34 +1,42 @@
-import { FC, ReactNode, useState } from "react";
+import { FC, ReactNode, useCallback, useState } from "react";
 import { GestureResponderEvent } from "react-native";
 import { Button, Dialog, Portal, Text } from "react-native-paper";
 
 export const useConfirmDeletion = () => {
   const [visible, setConfirmDeletionVisible] = useState(false);
+
   const ConfirmationDialog: FC<{
     children: ReactNode;
     onPress?: (e: GestureResponderEvent) => void;
-  }> = (props) => (
-    <Portal>
-      <Dialog
-        visible={visible}
-        onDismiss={() => setConfirmDeletionVisible(false)}
-      >
-        <Dialog.Title>Conferma cancellazione</Dialog.Title>
-        <Dialog.Content>
-          <Text variant="bodyMedium">{props.children}</Text>
-        </Dialog.Content>
-        <Dialog.Actions>
-          <Button
-            onPress={(e) => {
-              props.onPress && props.onPress(e);
-              setConfirmDeletionVisible(false);
-            }}
+  }> = useCallback(
+    (props) => {
+      return (
+        <Portal>
+          <Dialog
+            visible={visible}
+            onDismiss={() => setConfirmDeletionVisible(false)}
           >
-            Elimina
-          </Button>
-        </Dialog.Actions>
-      </Dialog>
-    </Portal>
+            <Dialog.Title>Conferma cancellazione</Dialog.Title>
+            <Dialog.Content>
+              <Text variant="bodyMedium">{props.children}</Text>
+            </Dialog.Content>
+            <Dialog.Actions>
+              <Button
+                mode="outlined"
+                onPress={(e) => {
+                  console.log("Cancel pressed");
+                  props.onPress && props.onPress(e);
+                  setConfirmDeletionVisible(false);
+                }}
+              >
+                Elimina {typeof props.onPress}
+              </Button>
+            </Dialog.Actions>
+          </Dialog>
+        </Portal>
+      );
+    },
+    [visible]
   );
   return {
     setConfirmDeletionVisible,
--- src/pages/Library/ViewSong.tsx
+++ src/pages/Library/ViewSong.tsx
@@ -1,66 +1,70 @@
 import { RouteProp, useNavigation, useRoute } from "@react-navigation/native";
 import { FC, useState } from "react";
-import { IconButton, Menu, Text } from "react-native-paper";
+import { Alert } from "react-native";
+import { IconButton, Menu } from "react-native-paper";
 import { Page } from "../../components/Page";
 import { SongDisplay } from "../../components/SongDisplay";
-import { useConfirmDeletion } from "../../hooks/useConfirmDeletion";
 import { useSongList } from "../../hooks/useSongList";
 import { LibraryTabScreenProps } from "../types";
 import { LibraryStackParams } from "./types";
 
 export const ViewSongMenu: FC = () => {
-  const { deleteSong, getSong } = useSongList();
+  const { deleteSong } = useSongList();
   const route = useRoute<RouteProp<LibraryStackParams, "View">>();
   const navigation = useNavigation();
 
-  const { setConfirmDeletionVisible, ConfirmationDialog } =
-    useConfirmDeletion();
   const [visible, setVisible] = useState(false);
 
   return (
-    <>
-      <Menu
-        visible={visible}
-        onDismiss={() => setVisible(false)}
-        anchorPosition="top"
-        anchor={
-          <IconButton
-            icon="dots-vertical"
-            mode="outlined"
-            onPress={() => setVisible(true)}
-          />
-        }
-      >
-        <Menu.Item
-          leadingIcon="pencil"
-          onPress={() => {
-            setVisible(false);
-            navigation.navigate("Library", {
-              screen: "Edit",
-              params: { song: route.params.song },
-            });
-          }}
-          title="Modifica"
+    <Menu
+      visible={visible}
+      onDismiss={() => setVisible(false)}
+      anchorPosition="top"
+      anchor={
+        <IconButton
+          icon="dots-vertical"
+          mode="outlined"
+          onPress={() => setVisible(true)}
         />
-        <Menu.Item
-          leadingIcon="delete"
-          onPress={() => {
-            setVisible(false);
-            setConfirmDeletionVisible(true);
-          }}
-          title="Elimina"
-        />
-      </Menu>
-      <ConfirmationDialog
-        onPress={() =>
-          deleteSong(route.params.song).then(() =>
-            navigation.navigate("Library", { screen: "List" })
-          )
-        }
-      >
-        <Text>Vuoi davvero eliminare questo pezzo dal repertorio?</Text>
-      </ConfirmationDialog>
-    </>
+      }
+    >
+      <Menu.Item
+        leadingIcon="pencil"
+        onPress={() => {
+          setVisible(false);
+          navigation.navigate("Library", {
+            screen: "Edit",
+            params: { song: route.params.song },
+          });
+        }}
+        title="Modifica"
+      />
+      <Menu.Item
+        leadingIcon="delete"
+        onPress={() => {
+          setVisible(false);
+          Alert.alert(
+            "Conferma cancellazione",
+            "Vuoi davvero eliminare questo pezzo dal repertorio?",
+            [
+              {
+                text: "Annulla",
+                style: "cancel",
+              },
+              {
+                text: "Elimina",
+                style: "destructive",
+                onPress: () =>
+                  deleteSong(route.params.song).then(() =>
+                    navigation.navigate("Library", { screen: "List" })
+                  ),
+              },
+            ]
+          );
+        }}
+        title="Elimina"
+      />
+    </Menu>
   );
 };
 
