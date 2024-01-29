2d373552ccc2fac0d8d2479da57c5993bdaf07b7
Franco Traversaro <franco.traversaro@nan.srl>
2024-01-29 19:54:14 +0100

    Try to use Camera

--- src/pages/Concert.tsx
+++ src/pages/Concert.tsx
@@ -1,42 +1,72 @@
-import { useNavigation } from "@react-navigation/native";
+import { Camera, CameraType } from "expo-camera";
+import { useState } from "react";
+import { StyleSheet, TouchableOpacity, View } from "react-native";
 import { Button, Text } from "react-native-paper";
 import { Page } from "../components/Page";
-import { Pentagram } from "../components/Pentagram";
-import { InitialNote, NoteName, Section } from "../types";
-
-type Short = [Section, number, NoteName];
-const convert = (...notes: Short[]) =>
-  notes.map(
-    ([section, octave, note]): InitialNote => ({
-      section,
-      subsection: 1,
-      note: {
-        note,
-        octave,
-        alteration: section === "tenori" ? "#" : "b",
-      },
-    })
-  );
 
 export const Concert = () => {
-  const aaa = useNavigation();
+  const [type, setType] = useState(CameraType.back);
+  const [permission, requestPermission] = Camera.useCameraPermissions();
+
+  if (!permission) {
+    // Camera permissions are still loading
+    return <View />;
+  }
+
+  if (!permission.granted) {
+    // Camera permissions are not granted yet
+    return (
+      <View style={styles.container}>
+        <Text style={{ textAlign: "center" }}>
+          We need your permission to show the camera
+        </Text>
+        <Button onPress={requestPermission}>grant permission</Button>
+      </View>
+    );
+  }
+
+  function toggleCameraType() {
+    setType((current) =>
+      current === CameraType.back ? CameraType.front : CameraType.back
+    );
+  }
   return (
     <Page>
-      <Text>Concert</Text>
-      <Text>TO DO!</Text>
-      <Pentagram
-        notes={convert(
-          ["tenori", 4, "C"],
-
-          ["bassi", 2, "A"]
-          // ["basso", 3, "C"],
-          // ["basso", 2, "C"],
-          // ["basso", 1, "C"]
-        )}
-      />
-      <Button onPress={() => aaa.navigate("Library" as unknown as never)}>
-        Libreria
-      </Button>
+      <View style={styles.container}>
+        <Camera style={styles.camera} type={type}>
+          <View style={styles.buttonContainer}>
+            <TouchableOpacity style={styles.button} onPress={toggleCameraType}>
+              <Text style={styles.text}>Flip Camera</Text>
+            </TouchableOpacity>
+          </View>
+        </Camera>
+      </View>
     </Page>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+  },
+  camera: {
+    flex: 1,
+  },
+  buttonContainer: {
+    flex: 1,
+    flexDirection: "row",
+    backgroundColor: "transparent",
+    margin: 64,
+  },
+  button: {
+    flex: 1,
+    alignSelf: "flex-end",
+    alignItems: "center",
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "white",
+  },
+});
